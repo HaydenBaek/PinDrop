902b7783931980a3159ed016a48e3b3c2ea580e8
Sophia Diluvio <sophia.diluvio@sap.com>
2025-07-11 22:24:26 -0700

    PINDROP-4_setup spring on vscode

---
+++ backend/src/main/java/com/example/pindrop/PindropApplication.java
@@ -0,0 +1,13 @@
+package com.example.pindrop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PindropApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PindropApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/pindrop/PindropApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pindrop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PindropApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
